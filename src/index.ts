55e26a0c281754dac22a906bb3f6c51e9e1e51b7
Netfloex <38650595+Netfloex@users.noreply.github.com>
2022-07-05 21:23:09 +0200

    Friendlier error message on db connect error

--- src/index.ts
+++ src/index.ts
@@ -13,14 +13,24 @@ const parseInteger = (int: string | undefined, or: number): number => {
 const port = parseInteger(process.env.PORT, 3000)
 const aedesPort = parseInteger(process.env.AEDES_PORT, 3001)
 
-AppDataSource.initialize().then(() => {
-	console.log("DataSource initialized")
+AppDataSource.initialize()
+	.then(() => {
+		console.log("DataSource initialized")
 
-	app.listen(port, () => {
-		console.log(chalk`{bold Express} listening on port {dim ${port}}`)
-	})
+		app.listen(port, () => {
+			console.log(chalk`{bold Express} listening on port {dim ${port}}`)
+		})
 
-	initializeAedes(aedesPort).then(() => {
-		console.log(chalk`{bold Aedes} listening on port {dim ${aedesPort}}`)
+		initializeAedes(aedesPort).then(() => {
+			console.log(
+				chalk`{bold Aedes} listening on port {dim ${aedesPort}}`,
+			)
+		})
 	})
-})
+	.catch((error) => {
+		if ("code" in error && error.code == "ECONNREFUSED") {
+			console.log(
+				chalk`{red ERROR} Could not connect to the database at {dim ${error.address}${error.port}}`,
+			)
+		}
+	})
